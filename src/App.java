2fcd0a1a699bd9b357bed4f9e4358a9c485cd601
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2024-01-24 09:08:04 +0100

    skapde vaiabler

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        variabler();
+    }
+
+    static void variabler(){
+        int livS = 10;
+        int livF = 10;
+        int skada1;
+        int skada2;
+        int scene;
     }
 }
+
+
