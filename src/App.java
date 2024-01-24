d04d6d07dbb9fd6f5322782dae91f3c3ca679907
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2024-01-24 09:33:47 +0100

    skapat 2 menyer och lagt till mer variabler

--- src/App.java
+++ src/App.java
@@ -2,8 +2,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+
+    static int scene;
     public static void main(String[] args) throws Exception {
         variabler();
+        if(scene == 1){
+            startmeny();
+        }
     }
 
     static void variabler(){
@@ -11,7 +16,24 @@ public class App {
         int livF = 10;
         int skada1;
         int skada2;
-        int scene;
+        scene = 1;
+        int difficulty;
+    }
+    static void startmeny(){
+        System.out.println(" - - - - - Fighter - - - - - ");
+        System.out.println(" - - - - - - - - - - - - - - ");
+        System.out.println(" - - - - 1. Starta - - - - - ");
+        System.out.println(" - - - 2. Iställningar - - - ");
+        System.out.println(" - - - - 3. Avsluta - - - - -");
+        System.out.println(" - - - - - - - - - - - - - - ");
+    }
+    static void instmeny(){
+        System.out.println(" - - - - - Fighter - - - - - ");
+        System.out.println(" - - - - - - - - - - - - - - ");
+        System.out.println(" - - - - 1. Normal - - - - - ");
+        System.out.println(" - - - - - 2. Svår - - - - - ");
+        System.out.println(" - - - - 3. Avsluta - - - - -");
+        System.out.println(" - - - - - - - - - - - - - - ");
     }
 }
 
